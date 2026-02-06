5a2c30a27a884f221ac52efb06045f8fceb89897
Naveen_02 <ny0919063@gmail.com>
2026-02-06 12:35:17 +0530

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,67 +1,62 @@
 import random
 import time
 import matplotlib.pyplot as plt
+def merge_sort(a):
+    n = len(a)
+    if(n>1):
+        b=a[0:n//2]
+        c=a[n//2:n]
+        print(b,"|",c)
+        merge_sort(b)
+        merge_sort(c)
+        merge(a,b,c)
 
+def merge(a, b, c):
+    n1 = len(b)
+    n2 = len(c)
 
+    i = j = k = 0
 
-
-# MERGE SORT 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-
-    return merge(left, right)
-
-
-
-
-def merge(left, right):
-    result = []
-    i = j = 0
-
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
+    while i < n1 and j < n2:
+        if b[i] < c[j]:
+            a[k] = b[i]
             i += 1
         else:
-            result.append(right[j])
+            a[k] = c[j]
             j += 1
+        k += 1
+
+    while i < n1:
+        a[k] = b[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        a[k] = c[j]
+        j += 1
+        k += 1
+
+    print(a)
 
 
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
+a=[6,4,2,8,9]
+merge_sort(a)
+print(a)
 
+n_list = [5000, 6000, 7000, 8000, 9000, 10000]
+sort_time = []
 
-arr = [38, 27, 43, 3, 9, 82, 10]
-sorted_arr = merge_sort(arr)
-print(sorted_arr)
+for n in n_list:
+    l = [random.randint(1, 100) for _ in range(n)]
 
+    s_t = time.time()
+    merge_sort(l)
+    e_t = time.time()
 
-# TIME ANALYSIS
-n_values = [5000, 10000, 15000, 20000, 25000, 30000]
-time_taken = []
+    sort_time.append(e_t - s_t)
 
+print("Input sizes:", n_list)
+print("Sorting times:", sort_time)
 
-for n in n_values:
-    arr = [random.randint(1, 100000) for _ in range(n)]
-
-
-    start_time = time.time()
-    merge_sort(arr)
-    end_time = time.time()
-
-
-    elapsed_time = end_time - start_time
-    time_taken.append(elapsed_time)
-
-
-    print(f"n = {n}, Time Taken = {elapsed_time:.6f} seconds")
- 
+plt.plot(n_list,sort_time)
+plt.show()
